a3de4bc851947ac15ca0a087763e394960f734dc
mubashirbappi <mubashirbappi@gmail.com>
2025-03-10 12:40:47 +0600

    added locomative scroll for batter  scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Canves from "./Canves";
 import data from "./assets/Data";
+import LocomotiveScroll from "locomotive-scroll";
 
 const App = () => {
+  useEffect(() => {
+    const scroll = new LocomotiveScroll();
+
+   
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
+
   return (
     <>
-     <h1 className="bg-red-400 text-white">Hello World</h1>
-    <div className="w-full min-h-screen bg-black relative">
-     
-     
-          {data[0].map((canvesdt, index) => (
-            <Canves key={index} canvesdt={canvesdt} />
-          ))}
-       
-    </div>
-    <div className="w-full min-h-screen bg-black relative">
-     
-     
-          {data[1].map((canvesdt, index) => (
-            <Canves key={index} canvesdt={canvesdt} />
-          ))}
-       
-    </div>
-    <div className="w-full min-h-screen bg-black relative">
-     
-     
-          {data[2].map((canvesdt, index) => (
-            <Canves key={index} canvesdt={canvesdt} />
-          ))}
-       
-    </div>
+      <h1 className="bg-red-400 text-white">Hello World</h1>
+      <div className="w-full min-h-screen bg-black relative">
+        {data[0].map((canvesdt, index) => (
+          <Canves key={index} canvesdt={canvesdt} />
+        ))}
+      </div>
+      <div className="w-full min-h-screen bg-black relative">
+        {data[1].map((canvesdt, index) => (
+          <Canves key={index} canvesdt={canvesdt} />
+        ))}
+      </div>
+      <div className="w-full min-h-screen bg-black relative">
+        {data[2].map((canvesdt, index) => (
+          <Canves key={index} canvesdt={canvesdt} />
+        ))}
+      </div>
     </>
   );
 };
