946d0fe90d090017f02f35e0d9a5fa7b23741843
mubashirbappi <mubashirbappi@gmail.com>
2025-03-10 12:23:26 +0600

    load all images

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import Canves from './Canves';
+import React from "react";
+import Canves from "./Canves";
+import data from "./assets/Data";
+
 const App = () => {
   return (
     <div>
       <h1 className="bg-red-400 text-white">Hello World</h1>
-      <Canves />
+      {data.map((item, index) => (
+        <div key={index}>
+          {item.map((canvesdt, index) => (
+            <Canves key={index} canvesdt={canvesdt} />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
--- src/Canves.jsx
+++ src/Canves.jsx
@@ -1,42 +1,55 @@
-import React, { useEffect, useRef, useState } from 'react';
-import canvasImg from './assets/CanvesImg';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-const Canves = () => {
-    const [index, setIndex] = useState({value: 150});
-    const canvasRef = useRef(null);
-    useGSAP(()=>{
-        gsap.to(index, {
-            value:700,
-            duration:3,
-            repeat: -1,
-            ease:'linear',
-            onUpdate:()=>{
-                setIndex({value: Math.round(index.value)})
-            }
-        })
-    },[index])
+import React, { useEffect, useRef, useState } from "react";
+import canvasImg from "./assets/CanvesImg";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+const Canves = ({ canvesdt }) => {
+  const { startIndex, numImages, duration, size, top, left, zIndex } = canvesdt;
+  const [index, setIndex] = useState({ value: startIndex });
+  const canvasRef = useRef(null);
+  useGSAP(() => {
+    gsap.to(index, {
+      value: startIndex + numImages - 1,
+      duration: duration,
+      repeat: -1,
+      ease: "linear",
+      onUpdate: () => {
+        setIndex({ value: Math.round(index.value) });
+      },
+    });
+  }, [index]);
 
+  useEffect(() => {
+    const scale = window.devicePixelRatio;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
 
-    useEffect(() => {
-        console.log(canvasImg);
-        const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
-        
-        const img = new Image();
-        img.src = canvasImg[index.value]; // Load first image from array
-        
-        img.onload = () => {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-        };
-    },[index])
-    return (
-        <canvas ref={canvasRef} id="canvas">
+    const img = new Image();
+    img.src = canvasImg[index.value]; // Load first image from array
 
-        </canvas>
-    );
+    img.onload = () => {
+      canvas.width = canvas.offsetWidth * scale;
+      canvas.height = canvas.offsetHeight * scale;
+      canvas.style.width = canvas.offsetWidth + "px";
+      canvas.style.height = canvas.offsetHeight + "px";
+
+      ctx.scale(scale, scale);
+      ctx.drawImage(img, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
+    };
+  }, [index]);
+  return (
+    <canvas
+      className="absolute"
+      style={{
+        width: `${size * 1.2}px`,
+        height: `${size * 1.2}px`,
+        top: `${top}%`,
+        left: `${left}%`,
+        zIndex: `${zIndex}`,
+      }}
+      ref={canvasRef}
+      id="canvas"
+    ></canvas>
+  );
 };
 
 export default Canves;
