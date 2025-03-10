04d72af65fb8aacac2b6361a11cec448c48a7b42
mubashirbappi <mubashirbappi@gmail.com>
2025-03-10 22:05:31 +0600

    hero section complate animated

--- src/App.jsx
+++ src/App.jsx
@@ -15,18 +15,18 @@ const App = () => {
 
   return (
     <>
-      <div className="w-full min-h-screen bg-black relative">
-        {/* {data[0].map((canvesdt, index) => (
+      <div className="w-full min-h-screen bg-black relative text-white">
+        {data[0].map((canvesdt, index) => (
           <Canves key={index} canvesdt={canvesdt} />
-        ))} */}
+        ))}
 
-        <div className="w-full h-screen bg-white">
-          <nav className="w-full h-20 flex items-center justify-between px-10">
-            <div className="text-black text-2xl font-semibold">Thirtysixstudio</div>
+        <div className="w-full h-screen  z-[1]  relative">
+          <nav className="w-full h-20 flex items-center justify-between text-white px-10">
+            <div className=" text-2xl font-semibold">Thirtysixstudio</div>
             <ul className="flex gap-8">
               {["Home", "About", "Services", "Projects", "Contact"].map((link, index) => (
                 <li key={index}>
-                  <a href={`#${link.toLowerCase()}`} className="text-black hover:text-gray-600 transition-colors">
+                  <a href={`#${link.toLowerCase()}`} className=" hover:text-gray-600 transition-colors">
                     {link}
                   </a>
                 </li>
@@ -34,16 +34,23 @@ const App = () => {
             </ul>
           </nav>
 
-          <div className="textcontainer w-full px-[20%] bg-red-500">
-           <div className="text w-[40%]">
-            <h3 className="text-4xl leading-[1.4]">
+          <div className="textcontainer w-full px-[20%]  ">
+           <div className="text w-[50%]">
+            <h3 className="text-3xl leading-[1.2]">
             At Thirtysixstudio, we build digital assets and immersive experiences for purposeful brands.
             </h3>
-            <p className="mt-10 text-md w-[80%]">
+            <p className="mt-8 text-md w-[80%]">
             We're a boutique production studio focused on design, animation, and technology, constantly rethinking what digital craft can do for present-day ads and campaigns.
             </p>
-            <p className="mt-10  text-xl">Scroll</p>
+            <p className="mt-8  text-xl">Scroll</p>
            </div>
+          </div>
+
+          <div className="w-full mt-20 bg-black text-white">
+            <h1 className="text-[13rem] font-normal tracking-tight leading-none">Thirtysixstudio</h1>
+          </div>
+          <div className="w-full h-screen bg-red-500">
+            <h1>Hello World</h1>
           </div>
 
         </div>
